b9699b72f6e6c842b73144cebef57cd7e990f8f5
Yesterday17 <t@yesterday17.cn>
2018-06-13 21:21:01 +0800

    fix d.ts file

--- lib/translate.d.ts
+++ lib/translate.d.ts
@@ -1,14 +1,16 @@
-/// <reference types="node" />
+// Type definitions for node-google-translate-china v1.1.2
+// Project: https://github.com/Modlate/node-google-translate-china
+// Definitions by: Yesterday17 <https://github.com/Yesterday17>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import translate = require("node-google-translate-skidz");
+export = node_google_translate_china;
 
-translate(
-    {
-        text: "text",
-        source: "en",
-        target: "zh"
-    },
-    result => {
-        console.log(result);
+declare function node_google_translate_china(opts: node_google_translate_china.InputOption, callback: (result: string) => void): void;
+
+declare namespace node_google_translate_china {
+    export interface InputOption {
+        text: string;
+        source: string;
+        target: string;
     }
-);
+}
